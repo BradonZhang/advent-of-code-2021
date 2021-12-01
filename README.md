5cc8ca17e12dd27bc7b71f1caf0e46e8a8dfa85e
Bradon Zhang <bradonzhang@gmail.com>
2021-12-01 05:07:10 -0500

    Add helper file for fetching input when run

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # advent-of-code-2021
 My solutions for Advent of Code 2021: https://adventofcode.com/2021
+
+Each solution consists of one file in the root folder, its name consisting of the day for the problem.
+
+`fetch_input.py` is a helper file that fetches and saves an input file when run. It requires a `.env` file to be created, which includes one field `AOC_SESSION`: the cookie session for fetching Advent of Code input. To run the file, please first install the necessary packages in `requirements.txt`.
---
+++ fetch_input.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+import dotenv
+import requests
+
+
+dotenv.load_dotenv()
+
+day = int(sys.argv[1] if len(sys.argv) >= 2 else input('Day to fetch: '))
+
+print(f'✔ Request being made for Day {day}')
+
+AOC_SESSION = os.getenv('AOC_SESSION')
+r = requests.get(f'https://adventofcode.com/2021/day/{day}/input', headers={
+    'cookie': f'session={AOC_SESSION}'
+})
+
+print(f'✔ File {day}.txt fetched')
+
+infile_path = f'in/{day}.txt'
+with open(infile_path, 'w') as f:
+    f.write(r.text)
+    print(f'✔ File saved as {infile_path}')
