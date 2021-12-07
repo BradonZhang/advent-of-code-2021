56768b4466adac95319d7cb118ee1834d6bfa595
Bradon Zhang <bradonzhang@gmail.com>
2021-12-07 00:20:40 -0500

    Change 7.1 to be one-liners

--- src/7.1.py
+++ src/7.1.py
@@ -4,18 +4,8 @@ with open('in/7.txt') as f:
 a = min(pos)
 b = max(pos)
 
-
 # Part 1
-
-best = float('inf')
-for i in range(a, b + 1):
-    best = min(best, sum(abs(p - i) for p in pos))
-print(best)
-
+print(min(sum(abs(p - i) for p in pos) for i in range(a, b + 1)))
 
 # Part 2
-
-best = float('inf')
-for i in range(a, b + 1):
-    best = min(best, sum((d := abs(p - i)) * (d + 1) // 2 for p in pos))
-print(best)
+print(min(sum((d := abs(p - i)) * (d + 1) // 2 for p in pos) for i in range(a, b + 1)))
